fb375525a970d77ecf71ebc4b8eacbf5897472f5
Nadir Sarigul <nadir.nyc62@gmail.com>
2019-01-30 06:11:05 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -17,14 +17,14 @@ function destructivelyAddElementToEndOfArray(array , element){
 function accessElementInArray(array, index){
   return array[index]
 }
-function destructivelyRemoveElementFromBeginningOfArray(array, element){
+function destructivelyRemoveElementFromBeginningOfArray(array){
   array.shift()
   array
 }
 function removeElementFromBeginningOfArray(array){
   return array.slice(1)
 }
- function destructivelyAddElementToEndOfArray(array){
+ function destructivelyRemoveElementFromBeginningOfArray(array){
    array.pop()
    return array
  }
