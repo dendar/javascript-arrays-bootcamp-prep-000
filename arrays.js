49acc2b3bf0cff3b8b1c2ebca5e0ad257d321802
Nadir Sarigul <nadir.nyc62@gmail.com>
2019-01-30 05:59:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,6 +11,6 @@ function addElementToEndOfArray(array,element){
   return[...array, element]
 }
 function destructivelyAddElementToEndOfArray(array , element){
-  array.push[element]
+  array.push(element)
   return array
 }
