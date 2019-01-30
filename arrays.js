21232cc37d3218e2232085c521b6be005841fb98
Nadir Sarigul <nadir.nyc62@gmail.com>
2019-01-30 06:50:40 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -28,10 +28,10 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
   array.shift()
   return array
 }
-//  function destructivelyRemoveElementFromEndOfArray(array){
-//    array.pop()
-//    return array
-//  }
+ function destructivelyRemoveElementFromEndOfArray(array){
+   array.pop()
+   return array
+ }
 //  function removeElementFromEndOfArray(array){
 //    return array.slice(0, array.length-1)
 //  }
